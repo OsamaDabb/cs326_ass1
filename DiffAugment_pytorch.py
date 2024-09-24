0e892d95540667e2865caa0bb5c6af71c5e42da4
Osama Dabbousi <osama.dabbousi@gmail.com>
2024-09-24 08:55:58 +0300

    added unit test

--- DiffAugment_pytorch.py
+++ DiffAugment_pytorch.py
@@ -40,6 +40,7 @@ def augment_contrast(x):
 
 def augment_translation(x, ratio=0.125):
     # Use torch RandomAffine to translate image up to 10% in all four directions
+    print(type(x))
     return torchvision.transforms.RandomAffine(degrees=0, translate=(1/8, 1/8))(x)
 
 
